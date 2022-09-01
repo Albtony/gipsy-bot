3b22bd3f07f584fdc3e8cf65fccf0ef677a8b5ab
albtony <albtony21@gmail.com>
2022-09-01 20:46:56 +0700

    remove try catch

--- utils/playMusic.js
+++ utils/playMusic.js
@@ -19,39 +19,30 @@ module.exports={
 	musicStart: async (bot, message) => {
 		const player = createAudioPlayer();
 
-		try {
-			// bisa refactor jadi shift() kalo queue.js ga butuh kaya gini.
-			let music = bot.musicQueue[0];
-			await player.play(getResource(music.url));
+		let music = bot.musicQueue[0];
+		await player.play(getResource(music.url));
 
-			player
-				.on('error', (error) => {
-					message.channel.send('something went wrong!');
-					console.log('error1: ');
-					console.error(error);
-				})
-				.addListener('stateChange', async (oldOne, newOne) => {
-					if (newOne.status == 'idle') {
-						message.channel.send('music finished');
-						bot.musicQueue.shift();
-						music = bot.musicQueue[0];
-						if (!music) {
-							message.channel.send('queue finished');
-							bot.voiceConnection.destroy();
-							bot.voiceConnection = null;
-							return;
-						}
-						player.play(getResource(music.url));
+		player
+			.on('error', (error) => {
+				message.channel.send('something went wrong!');
+				console.log('error1: ');
+				console.error(error);
+			})
+			.addListener('stateChange', async (oldOne, newOne) => {
+				if (newOne.status == 'idle') {
+					message.channel.send('music finished');
+					bot.musicQueue.shift();
+					music = bot.musicQueue[0];
+					if (!music) {
+						bot.voiceConnection.destroy();
+						bot.voiceConnection = null;
+						return;
 					}
-				});
+					player.play(getResource(music.url));
+				}
+			});
 				
-			bot.voiceConnection.subscribe(player);
-
-		} catch (error) {
-			message.channel.send('something went wrong!');
-			console.log('error2: ');
-			console.error(error);
-		}
+		bot.voiceConnection.subscribe(player);
 	}
 };
 
