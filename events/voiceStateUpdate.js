07b2eb12ad0be06695b89928471dbedc9d974fa1
albtony <albtony21@gmail.com>
2022-09-04 17:18:04 +0700

    remove debugging console.log statement

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'voiceStateUpdate',
 	execute: (bot) => {
 		if(!bot.voiceConnection || bot.voiceConnection._state.status == "disconnected") {
-			console.log('vsu');
             bot.musics.queue = [];
 			bot.musics.loop = false;
 			bot.musics.pos = 0;
