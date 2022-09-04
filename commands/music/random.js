3649416667a3d0821eecb510dc85e383c940d6e9
albtony <albtony21@gmail.com>
2022-09-04 17:37:33 +0700

    feat(music): add command to shuffle queue

---
+++ commands/music/random.js
@@ -0,0 +1,31 @@
+module.exports = {
+	name: 'shuffle',
+	aliases: [ 'sf' ],
+	description: 'shuffles queue',
+	ownerOnly: false,
+	hidden: false,
+	
+	run: (bot, message, args) => {
+        let musics = bot.musics;
+        musics.queue = shuffle(musics.queue);
+	}
+};
+
+function shuffle(queue) {
+    let currentMusic = queue.shift();
+    let currentIndex = queue.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+  
+        // And swap it with the current element.
+        [queue[currentIndex], queue[randomIndex]] = [queue[randomIndex], queue[currentIndex]];
+    }
+  
+    queue.unshift(currentMusic);
+    return queue;
+}
