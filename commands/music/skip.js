7ddf18aedb2331995ed69ddb55d711a3382e3006
albtony <albtony21@gmail.com>
2022-09-04 17:17:08 +0700

    feat(musics): add command to skip x music

---
+++ commands/music/skip.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'skip',
+	aliases: [ 's' ],
+	description: 'skips music',
+	ownerOnly: false,
+	hidden: false,
+	
+	run: (bot, message, args) => {
+        let musics = bot.musics;
+        let skip = args[0];
+
+        if (!skip) skip = 1;
+        if (!isNumeric(skip) || skip > musics.queue.length || skip <= 0) {
+            message.reply(`\`skip value is invalid\``);
+            return;
+        }
+            
+        if(skip == 1) message.channel.send(`\`skipping 1 song...\``);
+        else message.channel.send(`\`skipping ${skip} song...\``);
+        musics.skip = parseInt(skip);
+        musics.player.stop(true);
+	}
+};
+
+function isNumeric(str){
+    return /^\d+$/.test(str);
+}
